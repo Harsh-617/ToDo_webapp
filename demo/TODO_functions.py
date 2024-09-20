0570b2a77226616d9e34f83b1603f767164b05d5
Harsh-617 <harshkudu67@outlook.com>
2024-09-20 16:21:36 +0530

    web_app implementation

--- demo/TODO_functions.py
+++ demo/TODO_functions.py
@@ -1,9 +1,9 @@
 
-def get_todos(filepath = r"C:\Users\LENOVO-PC\PycharmProjects\harsh_charm\todo_gui"):
+def get_todos(filepath = r"TODO_list"):
     with open(filepath,'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath= r"C:\Users\LENOVO-PC\PycharmProjects\harsh_charm\todo_gui"):
+def write_todos(todos_arg, filepath= r"TODO_list"):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
